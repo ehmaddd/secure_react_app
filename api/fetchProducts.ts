365f52d619681e721f52a7b794b0727c698c1cea
ehmaddd <ehmaddd@gmail.com>
2024-01-16 23:23:06 +0500

    Update

--- api/fetchProducts.ts
+++ api/fetchProducts.ts
@@ -2,8 +2,21 @@ const nodeFetch = require('node-fetch');
 
 module.exports = async (req, res) => {
   try {
-    const response = await nodeFetch('https://dummyjsonapi.com/products');
+    console.log('Serverless function is running.');
+
+    // Set CORS headers to allow requests from any origin
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+
+    // Fetch data from the external API
+    const response = await nodeFetch('https://dummyjson.com/products');
     const data = await response.json();
+
+    // Log the response and data
+    console.log('Response:', response);
+    console.log('Data:', data);
+
+    // Send the data as JSON in the response
     res.status(200).json(data);
   } catch (error) {
     console.error('Error fetching data:', error);
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -14,10 +14,8 @@ const ProductList: React.FC = () => {
     const fetchProducts = async () => {
       try {
         const response = await fetch('api/fetchProducts');
-        const data = await response.json();
-        console.log(response);
-        console.log(data);
-        setProducts(data);
+        // const data = await response.json();
+        // setProducts(data);
       } catch (error) {
         console.error('Error fetching products:', error);
       }
