71620d754dbded71d91294e3d1686ea49c1047ef
ehmaddd <ehmaddd@gmail.com>
2024-01-16 21:46:46 +0500

    Update dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { clearUser } from '../redux/actions/authActions';
 import ProductList from './ProductList';
 
 const Dashboard: React.FC = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(clearUser());
+    navigate('/');
+  };
+
   return (
     <div>
+      <button onClick={handleLogout}>Logout</button>
       <h2>Dashboard</h2>
       <ProductList />
     </div>
